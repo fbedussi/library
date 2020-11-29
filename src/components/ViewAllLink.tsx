4c504fd47c750b6603a240d3bd39b82114f30a8b
Francesco Bedussi <francesco@francescobedussi.it>
2020-11-29 09:38:49 +0100

    fix: increase max counter

--- src/components/ViewAllLink.tsx
+++ src/components/ViewAllLink.tsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React from 'react';
+import { useSelector } from 'react-redux';
 
-import { selectBooks } from '../store/books/selectors'
-import { Badge, IconButton } from '../styleguide'
-import { Book } from '../styleguide/icons'
-import { LinkNoStyle } from './CommonComponents'
+import { selectBooks } from '../store/books/selectors';
+import { Badge, IconButton } from '../styleguide';
+import { Book } from '../styleguide/icons';
+import { LinkNoStyle } from './CommonComponents';
 
 const ViewAllLink = () => {
 	const books = useSelector(selectBooks);
 
 	return (
 		<LinkNoStyle to="/view-all">
+			IT
 			<IconButton color="inherit">
-				<Badge badgeContent={books.length} color="secondary">
+				<Badge badgeContent={books.length} color="secondary" max={9999}>
 					<Book />
 				</Badge>
 			</IconButton>
