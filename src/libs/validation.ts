5d3ea3f3c807be6d7ddfe9c2b830a2d298408d3e
Francesco Bedussi <francesco.bedussi@abstract.it>
2023-01-15 17:27:27 +0100

    fix: validation

--- src/libs/validation.ts
+++ src/libs/validation.ts
@@ -2,7 +2,10 @@ import { FormData } from '../model/model';
 import { TFunction } from 'i18next';
 
 export const bookFormValidation = (t: TFunction) => (values: FormData) => {
-	return Object.entries(values).reduce(errors => {
+	return Object.entries(values).reduce((errors, [key, val]) => {
+		if (key !== 'read' && !val) {
+			errors[key] = t('errors.mandatoryField');
+		}
 		return errors;
 	}, {} as { [k: string]: string });
 };
